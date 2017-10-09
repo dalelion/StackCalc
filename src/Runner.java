fc2a786b00b79296678533a1e8481c8c4595a440
Jacob Casey <caseyj5@wit.edu>
2017-10-09 14:28:12 -0400

    Jake Commit 2:28

--- src/Runner.java
+++ src/Runner.java
@@ -3,12 +3,119 @@ import java.util.Scanner;
 // (5 * (4 - 3) + 2)
 
 public class Runner {
+	
+	public static void main(String args[]){
+		StringBuilder k = new StringBuilder("jews");
+		k.replace(1, 2, "oa");
+		System.out.println(k.toString());
+		System.out.println(Evaluate("1+1*3"));
+		System.out.println(parenthEval("(1+1)+(3*2)"));
+	}
+	
+	public static String parenthEval(String input){
+//		VectorStack<String> stackedInput = new VectorStack<>(input.length());
+//		VectorStack<String> stackedInput2 = new VectorStack<>(input.length());
+//		VectorStack<String> stackedInput3 = new VectorStack<>(input.length());
+//		StringBuilder inBuild = new StringBuilder(input).reverse();
+//		while(inBuild.length() > 0){
+//			//int i = 0;
+//			for(int i = 0; i < inBuild.length(); i++){
+//				stackedInput.push(inBuild.substring(i, i+1));
+//			}
+//			while(stackedInput.peek() != ")"){
+//				stackedInput2.push(stackedInput.pop());
+//			}
+//			if(!stackedInput.isEmpty()){
+//				stackedInput2.push(stackedInput.pop());
+//			}
+//			while(stackedInput2.peek() != "("){
+//				stackedInput3.push(stackedInput2.pop());
+//			}
+//			if(!stackedInput2.isEmpty()){
+//				stackedInput3.push(stackedInput2.pop());
+//			}
+//			
+//			if(i == inBuild.length()){
+//				return Evaluate(stackedInput);
+//			}
+//			else{
+//				stackedInput.push(inBuild.substring(i, i+1));
+//				while(stackedInput.peek().charAt(0) != '('){
+//					stackedInput2.push(stackedInput.pop());
+//				}
+//				stackedInput2.push(stackedInput.pop());
+//				while(stackedInput)
+//			}
+//		}
+		boolean parenthetical = false;
+		int parenRight = 0;
+		int parenLeft = 0;
+		StringBuilder master = new StringBuilder(input);
+		for(int i = 0; i < input.length(); i++){
+			if(master.charAt(i) == ')'){
+				parenRight = i;
+				parenthetical = true;
+				break;
+			}
+		}
+		
+		if(!parenthetical){
+			//return Evaluate(input).toString();
+			return Evaluate(master.toString()).toString();
+		}
+		
+		
+		else{
+			while(master.length() > 1){
+				parenLeft = 0;
+				parenRight = 0;
+				parenthetical = false;
+				for(int i = 0; i < master.length(); i++){
+					if(master.charAt(i) == ')'){
+						parenRight = i;
+						parenthetical = true;
+						break;
+					}
+				}
+				if(!parenthetical){
+					return Evaluate(master.toString()).toString();
+				}
+				else{
+					StringBuilder out = new StringBuilder("");
+					for(int i = parenRight; i >= 0; i--){
+						if(master.charAt(i) == '('){
+							parenLeft = i;
+							break;
+						}
+					}
+					System.out.println(parenRight); // test parenRight
+					System.out.println(parenLeft); // test parenLeft
+					for(int i = parenLeft+1; i < parenRight-1; i++){ // 5 + (5 * 2)
+						out.append(master.charAt(i));
+					}
+					Integer replace = Evaluate(out.toString()); // (5 * 2) = 10
+					for(int i = parenLeft+1; i <= parenRight; i++){ // 5 + ()
+						master.deleteCharAt(i);
+					}
+					master.replace(parenLeft, parenLeft+replace.toString().length()-1, replace.toString()); // 5 + 10
+				}
+			}
+			return master.toString();
+		}
+			/*
+			 * stack1 -> (5*(4+5)-3)
+			 * 
+			 * push onto stack2 -> (5*(4+5)
+			 * push back onto stack1 until ( -> stack1 = (5+4)-3)
+			 * stack3 pulls out just simple parenthetical expression -> stack3 = (4+5)
+			 */
+	}
 
-	public static void main(String args[]) {
+	public static Integer Evaluate(String x) {
 
 		VectorStack<Expression> ExpressionStack = new VectorStack<Expression>();
 
-		String x = "3";
+//		String x = "3";
 		Scanner stream = new Scanner(x);
 		stream.useDelimiter("");
 
@@ -62,8 +169,8 @@ public class Runner {
 
 		}
 
-		//return ex.getValue();
-		System.out.println(ex.getValue());
+		return ex.getValue();
+//		System.out.println(ex.getValue());
 
 	}
 }
